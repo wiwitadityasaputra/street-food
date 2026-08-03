399131e40a2a0971237b512750d376aa584406f5
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2026-08-04 02:23:05 +0700

    Using fontawsome from dependency

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import Image from 'next/image';
 import React from 'react';
 import clsx from 'clsx';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faUpwork } from "@fortawesome/free-brands-svg-icons";
+import { faBars, faTimes, faUser } from '@fortawesome/free-solid-svg-icons';
+
 export default function Home() {
   let [collapse, setCollapse] = React.useState(true);
 
   function collapseMenuClicked() {
     setCollapse(!collapse);
-    console.log("dgb click ", collapse)
   }
 
   return (
@@ -28,10 +31,16 @@ export default function Home() {
                 </div>
                 <div className="col-xl-6 col-sm-6 col-md-4">
                     <ul className="topbar_icon d-flex flex-wrap">
-                        <li><a href="#"><i className="fab fa-facebook-f" aria-hidden="true"></i></a> </li>
-                        <li><a href="#"><i className="fab fa-twitter" aria-hidden="true"></i></a> </li>
-                        <li><a href="#"><i className="fab fa-linkedin-in" aria-hidden="true"></i></a> </li>
-                        <li><a href="#"><i className="fab fa-behance" aria-hidden="true"></i></a> </li>
+                        <li>
+                          <a href="#">
+                            <FontAwesomeIcon icon={faUpwork} size="sm" />
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <FontAwesomeIcon icon={faGithub} size="sm" />
+                          </a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -51,32 +60,34 @@ export default function Home() {
             </a>
 
             <button className={clsx('navbar-toggler', {'show': !collapse})} type="button" onClick={collapseMenuClicked}>
-              <i className="far fa-bars menu_icon_bar" aria-hidden="true"></i>
-              <i className="far fa-times close_icon_close" aria-hidden="true"></i>
+              { collapse && <FontAwesomeIcon icon={faBars} size="sm" />}
+              { !collapse && <FontAwesomeIcon icon={faTimes} size="sm" />}
             </button>
                           
             <div className={clsx('navbar-collapse', {
               'collapse': collapse
             })}>
-                <ul className="navbar-nav m-auto">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="index.html">Menu</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="about.html">Checkout</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="menu.html">Order</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="chefs.html">Queue</a>
-                    </li>
-                </ul>
-                <ul className="menu_icon d-flex flex-wrap">
-                    <li>
-                        <a href="dashboard.html"><i className="fas fa-user" aria-hidden="true"></i></a>
-                    </li>
-                </ul>
+            <ul className="navbar-nav m-auto">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="index.html">Menu</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="about.html">Checkout</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="menu.html">Order</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="chefs.html">Queue</a>
+                </li>
+            </ul>
+            <ul className="menu_icon d-flex flex-wrap">
+                <li>
+                    <a href="dashboard.html">
+                      <FontAwesomeIcon icon={faUser} size="sm" />
+                    </a>
+                </li>
+            </ul>
             </div>
         </div>
       </nav>
