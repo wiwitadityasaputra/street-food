ae0ac520c9fe6152c304fd19fd7fab1b49f4b635
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2026-08-08 20:18:45 +0700

    Update cookies after delete user cart

--- app/lib/form-action/cart.action.ts
+++ app/lib/form-action/cart.action.ts
@@ -1,11 +1,17 @@
 "use server"
+
 import { redirect } from 'next/navigation';
 
-import { deleteUserCartByUserAndUserCartId } from "../database/database";
+import { countUserCartByUserAndFlag, deleteUserCartByUserAndUserCartId } from "../database/database";
+import { cookiesSetUserIdAndTotalCart } from '../util/cookie-util';
 
 export async function deleteCartItemAction(formData: FormData): Promise<void> {
     const userId = String(formData.get("userId"));
     const userCartId = String(formData.get("userCartId"));
     await deleteUserCartByUserAndUserCartId(userId, userCartId);
+
+    const totalCart = await countUserCartByUserAndFlag(userId, "active");
+    await cookiesSetUserIdAndTotalCart(userId, totalCart);
+
     redirect(`/dashboard/cart`);
 }
